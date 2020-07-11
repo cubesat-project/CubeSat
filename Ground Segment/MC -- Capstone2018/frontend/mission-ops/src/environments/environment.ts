03e90f2a66d83a4d1747cded3a7a92d660a80cbc
asahapde <60831459+asahapde@users.noreply.github.com>
2020-07-10 22:52:52 -0400

    Add node modules, environment.ts, and node-sass

---
+++ Ground Segment/MC -- Capstone2018/frontend/mission-ops/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    apiRouteBase: 'http://localhost:3000',
+    cognito: {
+      accessKeyId: 'AKIAQCM2BA4DZ4B3GDOV',
+      secretAccessKey: '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',
+      region: 'us-east-2',
+      userPoolId: 'us-east-2_YZSlXzFlB',
+      userPoolClientId: '1bn896ocv98neen3r2djed4r7i'
+    },
+    groundStationIP: 'http://0.0.0.0:3700'
+  };
+  
